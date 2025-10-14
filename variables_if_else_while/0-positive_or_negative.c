b4f205842d92735bcedd86754041a4c95e2c6f2d
root <hri7isd@gmail.com>
2025-10-14 10:22:46 +0000

    dsd

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - talks about if
+* Return: 0 is succes
+*betty style doc for function main goes there
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n == 0)
+{
+printf("%d is zero", n)
+}
+else if (n > 0)
+{
+printf("%d is positive", n)
+}
+else
+(n < 0)
+{
+printf("%d is negative", n)
+}
+
+	return (0);
+}
