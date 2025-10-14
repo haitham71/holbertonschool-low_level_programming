1fb248800afab3ccf84a182efb3aabb9266d318e
root <hri7isd@gmail.com>
2025-10-14 11:48:56 +0000

    dasda

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - talks about if
+* Return: 0 is succes
+*betty style doc for function main goes there
+*/
+int main(void)
+{
+char letters;
+
+for (letters = 'a' letter <= 'z' ; letter++)
+{
+putchar(letrres);
+}
+putchar(\n");
+
+return (0);
+}
