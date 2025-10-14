84f3d80b49505e5de4f6e4ce36135922b7b8fdb9
root <hri7isd@gmail.com>
2025-10-14 14:29:47 +0000

    fsf

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - talks about if
+* Return: 0 is succes
+*betty style doc for function main goes there
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
