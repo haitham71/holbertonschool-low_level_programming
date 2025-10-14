12ea46b775146fd94c256568220e283f6aa2d3f0
root <hri7isd@gmail.com>
2025-10-14 12:31:48 +0000

    dsdsd

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - function
+* Return: 0 is succes
+*betty style doc for function main goes there
+*/
+int main(void)
+{
+int nums;
+
+for (nums = 0; nums <= 9 ; nums++)
+
+printf("%d", nums);
+printf('\n');
+return (0);
+}
