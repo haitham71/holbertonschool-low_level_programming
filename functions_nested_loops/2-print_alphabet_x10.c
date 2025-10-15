09e061040a8cd9339057b72cc9f31b25945ad897
root <hri7isd@gmail.com>
2025-10-15 11:50:26 +0000

    sdsd

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - it's a func called from main.c
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int i = 0;
+
+while (i < 10)
+{
+
+
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+i++;
+}
+}
+
