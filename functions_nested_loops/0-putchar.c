dd3e7bb4228ee252953c2c4d962418236225066f
root <hri7isd@gmail.com>
2025-10-15 10:40:39 +0000

    as

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(void){
-int i ;
-char h [] = "_putchar";
-for ( i = 0; i <= 7; i++ )
+int main(void)
+{
+int i;
+char h[] = "_putchar";
+for (i = 0; i <= 7; i++)
 
 _putchar(h[i]);
 
 _putchar('\n');
-return 0;
+
+return (0);
 
 }
