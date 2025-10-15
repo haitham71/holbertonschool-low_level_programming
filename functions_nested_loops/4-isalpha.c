51c5bdbb3ec31a127c6fa59c848457e9a2a30749
root <hri7isd@gmail.com>
2025-10-15 13:28:20 +0000

    ada

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
+
+
