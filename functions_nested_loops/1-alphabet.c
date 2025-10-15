39c5040aca6fcbf97d82fc024f099609e3167392
root <hri7isd@gmail.com>
2025-10-15 11:35:55 +0000

    sds

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - it's a func called from main.c
+ */
+void print_alphabet(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+
+_putchar('\n');
+}
+
