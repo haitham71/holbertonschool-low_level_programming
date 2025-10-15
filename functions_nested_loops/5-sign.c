25d685e39059350657f5dade470c7481f2bc53b6
root <hri7isd@gmail.com>
2025-10-15 13:50:37 +0000

    sss

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the nums
+ * @n: the num to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+
+else if (n == 0)
+{
+_putchar ('0');
+return (0);
+}
+
+else
+{
+_putchar ('-');
+return (-1);
+}
+}
