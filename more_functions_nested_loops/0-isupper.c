fa54e0c60eb1ce2ad2301ed4f91d098380d2db44
root <hri7isd@gmail.com>
2025-10-20 10:21:31 +0000

    sdfs

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <ctype.h>
-/**
-* _isupper - will check if the char in upper or lower case
-* return -
-*/
 
+/**
+ * _isupper - checks if a character is an uppercase letter
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
-
 if (isupper(c))
-return(1);
+return (1);
 else
-return(0);
+return (0);
 }
