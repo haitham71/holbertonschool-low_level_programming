023455db4fb1976bd6a49534cda944f49351ed68
root <hri7isd@gmail.com>
2025-10-20 11:12:31 +0000

    a

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ *
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+char i, n;
+{
+for (i = '0'; i <= '1'; i++)
+_putchar(i);
+_putchar('3');
+}
+{
+for (n = '5'; n <= '9'; n++)
+_putchar(n);
+}
+_putchar('\n');
+}
