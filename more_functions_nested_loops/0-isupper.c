61981c5a9d983689a503f261167257dffe2a5e06
root <hri7isd@gmail.com>
2025-10-20 10:17:38 +0000

    sdfs

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+* _isupper - will check if the char in upper or lower case
+* return -
+*/
+
+int _isupper(int c)
+{
+
+if (isupper(c))
+return(1);
+else
+return(0);
+}
