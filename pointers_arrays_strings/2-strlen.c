71dbdb72e2a53357325cc0df29f81cf076014d73
root <hri7isd@gmail.com>
2025-10-24 18:03:14 +0000

    a

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ * Return: The length of the string
+*/
+
+int _strlen(char *s)
+{
+
+return (strlen(s));
+}
