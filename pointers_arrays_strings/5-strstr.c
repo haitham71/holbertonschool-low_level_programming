b7c726bf869a7bface0eb7b25f89fa664e05d25e
root <hri7isd@gmail.com>
2025-10-27 13:56:17 +0000

    a

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * *_strstr - searches a string for any of a set of bytes
+ * @needle: string to scan
+ * @haystack: string containing characters to match
+ *
+ * Return: pointer to the first occurrence in s of any byte in accept,
+ * or NULL if no such byte is found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != '\0'; i++)
+{
+if (haystack[i] == needle[j])
+return (&haystack[i]);
+}
+}
+return (NULL);
+}
+
+
