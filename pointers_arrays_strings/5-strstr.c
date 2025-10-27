796cbf20f7c8e2dec21b16deee8a3d11d721fb89
root <hri7isd@gmail.com>
2025-10-27 14:11:02 +0000

    a

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -14,13 +14,14 @@ char *_strstr(char *haystack, char *needle)
 int i, j;
 for (i = 0; haystack[i] != '\0'; i++)
 {
-for (j = 0; needle[j] == haystack[i]; j++)
+ for (j = 0; needle[j] != '\0'; j++)
 {
-if (haystack[i] == needle[j])
-return (&haystack[i]);
+if (haystack[i + j] != needle[j])
+break;
 }
+if (needle[j] == '\0')
+return (&haystack[i]);
 }
 return (NULL);
 }
-
 
