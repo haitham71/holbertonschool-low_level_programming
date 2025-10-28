95693d91a23088ab8e4962a4647e4a0553f15a0d
haitham71 <hri7isd@gmail.com>
2025-10-28 17:17:57 +0300

    ss

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,11 +2,7 @@
 
 int _strlen_recursion(char *s)
 {
-int i = 0;
-
-while (s[i] != '\0')
-{
-i++;
-}
-return i;
+    if (*s == '\0')
+        return (0);
+    return (1 + _strlen_recursion(s + 1));
 }
