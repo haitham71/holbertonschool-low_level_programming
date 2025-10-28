5dc77702a9cd001b40d9aaf48128eb4d1f31ec0d
haitham71 <hri7isd@gmail.com>
2025-10-28 20:11:41 +0300

    101 fixed

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -17,7 +17,7 @@ if (*(s2 + 1) == '\0')
 return (1);
 if (wildcmp(s1, s2 + 1))
 return (1);
-if (*s1 != '\0' && wildcmp(s1 + 1, s2))\
+if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 return (1);
 return (0);
 }
