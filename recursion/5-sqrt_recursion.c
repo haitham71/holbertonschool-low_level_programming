0ae2ceefc324feff0d93e911051bb6407afdf005
haitham71 <hri7isd@gmail.com>
2025-10-28 19:28:05 +0300

    ss

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 17)
+{
+return -1;
+}
+else
+{
+return (sqrt(n));
+}
+
+}
