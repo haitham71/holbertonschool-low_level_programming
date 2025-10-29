dda8c0eba14cc9fc799910f5c2422ba2d77e8bb0
haitham71 <hri7isd@gmail.com>
2025-10-29 18:43:21 +0300

    GG!

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,6 +3,7 @@
  * main - Prints the name of the program followed by a new line.
  * @argc: The number of command-line arguments.
  * @argv: An array containing the command-line arguments.
+ * Return: Always 0 (Success).
  */
 int main(int argc, char *argv[])
 {
