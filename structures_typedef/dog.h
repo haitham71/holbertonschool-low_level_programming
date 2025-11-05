e58d984f3e82189f2db5e376c88c81512aa07c05
haitham71 <hri7isd@gmail.com>
2025-11-05 11:42:01 +0300

    aa

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - structure representing information about a dog
- * struct - structure representing information about a dog
  * @name: pointer to the dog's name
  * @age: dog's age
  * @owner: pointer to the dog's owner name
@@ -12,17 +12,17 @@
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-typedef struct
-{
-char *name;
-float age;
-char *owner;
-} dog_t;
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 #endif
