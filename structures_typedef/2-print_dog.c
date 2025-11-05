c38468184c9803e4fe3cc354bbeb8bdd72d836a2
haitham71 <hri7isd@gmail.com>
2025-11-05 10:51:14 +0300

    fixes

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,13 +15,13 @@ return;
 if (d->name == NULL)
 printf("Name: (nil)\n");
 else
-printf("Name: %s\n", d -> name);
+printf("Name: %s\n", d->name);
 
-printf("Age: %f\n", d -> age);
+printf("Age: %f\n", d->age);
 
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
-
-printf("Owner: %s\n", d -> owner);
+else
+printf("Owner: %s\n", d->owner);
 
 }
