a53b75d2fa079328ccffbea8bffdd565847810b3
haitham71 <hri7isd@gmail.com>
2025-11-11 15:45:12 +0300

    jk

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name using a given function
+ * @name: name of the person
+ * @f: pointer to the function that prints the name
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *)){
+
+f (name);
+}
