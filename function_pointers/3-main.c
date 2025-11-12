d289aef0c52e5bf4bdc332b0750c8d4d36064ca2
haitham71 <hri7isd@gmail.com>
2025-11-12 14:31:48 +0300

    hh

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,7 +13,7 @@
 int main(int argc, char *argv[])
 {
 int num1, num2;
-int (*operation)(int, int);
+int (*func)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
@@ -21,15 +21,17 @@ exit(98);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
-operation = get_op_func(argv[2]);
-if (operation == NULL)
+func = get_op_func(argv[2]);
+if (func == NULL)
 {
 printf("Error\n");
 exit(99);
 }
-if ((strcmp(argv[2], "/") == 0 && num2 == 0)
-|| (strcmp(argv[2], "%") == 0 && num2 == 0))
+if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 {
 printf("Error\n");
 exit(100);
+}
+printf("%d\n", func(num1, num2));
+return (0);
 }
