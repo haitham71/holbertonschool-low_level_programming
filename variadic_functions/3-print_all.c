97fbda89c6d2145bfd580ee5c104b3eb4239d4c3
haitham71 <hri7isd@gmail.com>
2025-11-13 19:32:39 +0300

    beat bdr down

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
-
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
@@ -12,7 +11,6 @@ unsigned int i = 0;
 char *str;
 char *sep = "";
 va_list args;
-int first = 1;  /* شرط واحد لمعرفة أول عنصر */
 
 va_start(args, format);
 
@@ -21,33 +19,25 @@ while (format && format[i])
 switch (format[i])
 {
 case 'c':
-if (!first)
-printf(", ");
-printf("%c", va_arg(args, int));
-first = 0;
+printf("%s%c", sep, va_arg(args, int));
 break;
 case 'i':
-if (!first)
-printf(", ");
-printf("%d", va_arg(args, int));
-first = 0;
+printf("%s%d", sep, va_arg(args, int));
 break;
 case 'f':
-if (!first)
-printf(", ");
-printf("%f", va_arg(args, double));
-first = 0;
+printf("%s%f", sep, va_arg(args, double));
 break;
 case 's':
 str = va_arg(args, char *);
 if (str == NULL)
 str = "(nil)";
-if (!first)
-printf(", ");
-printf("%s", str);
-first = 0;
+printf("%s%s", sep, str);
 break;
+default:
+i++;
+continue;
 }
+sep = ", ";
 i++;
 }
 printf("\n");
