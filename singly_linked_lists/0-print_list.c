16b196d578a1e5d0a110e29e987d6df6a28b5072
haitham71 <hri7isd@gmail.com>
2025-11-25 14:14:03 +0300

    Fix: Betty Requirment

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,6 +3,7 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
+ * 
  * Return: the number of nodes in the list
  */
 size_t print_list(const list_t *h)
