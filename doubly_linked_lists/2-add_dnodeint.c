1031bab7d9a69ef21c2c1491d5d8e717f2e736ab
haitham71 <hri7isd@gmail.com>
2025-12-02 14:22:04 +0300

    fix the error

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -2,28 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list
  * @head: pointer to the head of the list
  * @n: integer to be added to the new node
  * Return: the address of the new node, or NULL if it failed
  */
-size_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new_node;
+    dlistint_t *new_node;
 
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (0);
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-new_node->n = n;
-new_node->prev = NULL;
-new_node->next = *head;
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = *head;
 
-if (*head != NULL)
-(*head)->prev = new_node;
+    if (*head != NULL)
+        (*head)->prev = new_node;
 
-*head = new_node;
+    *head = new_node;
 
-return (new_node);
+    return (new_node);
 }
