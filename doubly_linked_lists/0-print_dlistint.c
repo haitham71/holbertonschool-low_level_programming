b3ae5f85b89532b7237d7bbc06f06615e82c3165
haitham71 <hri7isd@gmail.com>
2025-12-02 14:07:16 +0300

    fixing betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -7,14 +7,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t i = 0;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        i++;
-        h = h->next;
-    }
-
-    return (i);
+size_t i = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+i++;
+h = h->next;
+}
+return (i);
 }
