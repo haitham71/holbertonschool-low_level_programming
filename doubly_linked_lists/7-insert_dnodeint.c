f087532b5194e508fbfc3a6703ecf0404e100369
haitham71 <hri7isd@gmail.com>
2025-12-03 19:56:00 +0300

    jj

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,8 +15,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	dlistint_t *temp = *h, *realnode = *h;
+	
 	if (h == NULL)
-
 		return (NULL);
 	realnode = malloc(sizeof(dlistint_t));
 	if (realnode == NULL)
