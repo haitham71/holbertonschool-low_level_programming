ed5fc292345d2796edb32025700422ec476ffa91
haitham71 <hri7isd@gmail.com>
2025-12-08 14:35:22 +0300

    betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,10 +1,11 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
+ * key_index - gives the index of a key in a hash table
+ * @key: the key
+ * @size: size of the hash table array
  *
- * Return: pointer to the newly created hash table, or NULL on failure
+ * Return: the index at which the key/value pair should be stored
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
