5447655433932827e37732ae5df975403001f3dc
haitham71 <hri7isd@gmail.com>
2025-12-10 13:39:51 +0300

    check

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,17 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *h;
+    hash_table_t *h;
 
-	h = malloc(sizeof(hash_table_t));
-	if (h == NULL)
-		return (NULL);
-	h->size = size;
-	h->array = malloc(sizeof(hash_node_t *) * size);
-	if (h->array == NULL)
-	{
-		free(h);
-		return (NULL);
-	}
-	return (h);
+    h = malloc(sizeof(hash_table_t));
+    if (h == NULL)
+        return (NULL);
+
+    h->size = size;
+    h->array = calloc(size, sizeof(hash_node_t *));
+    if (h->array == NULL)
+    {
+        free(h);
+        return (NULL);
+    }
+
+    return (h);
 }
+
--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,36 +12,51 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new, *temp;
+    unsigned long int index;
+    hash_node_t *new, *temp;
 
-	if (ht == NULL || key == NULL || value == NULL)
-		return (0);
-	index = key_index((unsigned char *)key, ht->size);
-	temp = ht->array[index];
-	while (temp != NULL)
-	{
-		if (strcmp(temp->key, key) == 0)
-		{
-			free(temp->value);
-			temp->key = strdup(key);
-			temp->value = strdup(value);
-			if (temp->value == NULL)
-				return (0);
-			return (1);
-		}
-		temp = temp->next;
-	}
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
-	new->key = strdup(key);
-	if (new->key == NULL)
-		return (0);
-	new->value = strdup(value);
-	if (new->value == NULL)
-		return (0);
-	new->next = ht->array[index];
-	ht->array[index] = new;
-	return (1);
+    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+        return (0);
+
+    index = key_index((unsigned char *)key, ht->size);
+    temp = ht->array[index];
+
+    while (temp)
+    {
+        if (strcmp(temp->key, key) == 0)
+        {
+            free(temp->value);
+            free(temp->key);
+
+            temp->key = strdup(key);
+            temp->value = strdup(value);
+
+            if (!temp->key || !temp->value)
+                return (0);
+
+            return (1);
+        }
+        temp = temp->next;
+    }
+
+    new = malloc(sizeof(hash_node_t));
+    if (!new)
+        return (0);
+
+    new->key = strdup(key);
+    new->value = strdup(value);
+
+    if (!new->key || !new->value)
+    {
+        free(new->key);
+        free(new->value);
+        free(new);
+        return (0);
+    }
+
+    new->next = ht->array[index];
+    ht->array[index] = new;
+
+    return (1);
 }
+
