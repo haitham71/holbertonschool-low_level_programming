59c67a0cbfd7eec126abc661a1648807b0ca51e1
haitham71 <hri7isd@gmail.com>
2025-12-10 13:26:02 +0300

    delete it

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,23 +9,23 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *temp;
 	unsigned long int i;
-	int first = 1;
+	int flag = 0;
+	hash_node_t *temp;
 
 	if (ht == NULL)
-		printf("{}");
+		return;
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		temp = ht->array[i];
-		while (temp != NULL)
+		while (temp)
 		{
-			if (!first)
+			if (flag == 1)
 				printf(", ");
 			printf("'%s': '%s'", temp->key, temp->value);
-			first = 0;
+			flag = 1;
 			temp = temp->next;
 		}
 	}
