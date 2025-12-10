c59dd2a2b35b240eb9e5a4c74ae725eb801615e4
haitham71 <hri7isd@gmail.com>
2025-12-10 13:28:05 +0300

    check

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -8,22 +8,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *node, *temp;
 	unsigned long int i;
-	hash_node_t *temp, *current;
 
 	if (ht == NULL)
 		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		current = ht->array[i];
-		while (current != NULL)
+		node = ht->array[i];
+
+		while (node != NULL)
 		{
-			temp = current->next;
-			free(current->key);
-			free(current->value);
-			free(current);
-			current = temp;
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
 		}
 	}
 	free(ht->array);
